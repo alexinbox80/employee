56d57fdf3f718730acf99c6cb75f7935dadda63e
AlexeySerov <alexinbox80@inbox.ru>
2025-07-25 12:58:06 +0300

    Add admin dashboard skeleton

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+//    Route::get('/', AdminIndexController::class)
+//        ->name('index');
+    Route::get('/', function() {
+        return view('admin.index');
+    })->name('index');
+});
